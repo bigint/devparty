c442c3142803c47c8a6c5d41ea7a2b9b0a8f9cfa
Yogi <yoginth@hey.com>
2021-09-14 12:28:25 +0530

    remove: Purger

--- src/graphql/resolvers/Post/mutations/deletePost.ts
+++ src/graphql/resolvers/Post/mutations/deletePost.ts
@@ -2,9 +2,6 @@ import { Session } from '@prisma/client'
 
 import { EditPostInput } from '~/__generated__/schema.generated'
 import { db } from '~/utils/prisma'
-import { purgeLikes } from '~/utils/purger/purgeLikes'
-import { purgeReplies } from '~/utils/purger/purgeReplies'
-import { purgeTopics } from '~/utils/purger/purgeTopics'
 
 export const deletePost = async (
   query: any,
@@ -20,11 +17,6 @@ export const deletePost = async (
 
     rejectOnNotFound: true
   })
-
-  // Purge all related data
-  await purgeLikes({ postId: post?.id })
-  await purgeReplies({ parentId: post?.id })
-  await purgeTopics({ postId: post?.id })
 
   return await db.post.delete({
     where: { id: post?.id }
