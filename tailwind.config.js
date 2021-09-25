6a4c3dbb2e4c2e30e7ff83b54bc08cec9f4ca606
Yogi <yoginth@hey.com>
2021-09-25 16:27:55 +0000

    add: Colors file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,32 @@
 const colors = require('tailwindcss/colors')
-const brandColors = require('./src/colors.ts')
 
 module.exports = {
   mode: 'jit',
   purge: ['./src/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
-    colors: brandColors,
+    colors: {
+      gray: {
+        0: '#fafbfc',
+        50: '#fafbfc',
+        100: '#f6f8fa',
+        200: '#e1e4e8',
+        300: '#d1d5da',
+        400: '#959da5',
+        500: '#6a737d',
+        600: '#586069',
+        700: '#444d56',
+        800: '#2f363d',
+        900: '#24292e'
+      },
+      black: colors.black,
+      white: colors.white,
+      purple: colors.violet,
+      pink: colors.pink,
+      oragne: colors.oragne,
+      yellow: colors.yellow,
+      green: colors.green
+    },
     extend: {
       colors: {
         brand: colors.violet
