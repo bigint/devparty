6ab60cb24a26feb7cc1bd4c8ef0226fd23fdd845
Yogi <yoginth@hey.com>
2021-09-25 05:48:28 +0000

    add: Sentry

--- next.config.js
+++ next.config.js
@@ -1,11 +1,24 @@
-module.exports = {
+const { withSentryConfig } = require('@sentry/nextjs')
+
+const moduleExports = {
   reactStrictMode: true,
   experimental: { esmExternals: true },
+  target: 'experimental-serverless-trace',
   env: {
     SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
     BASE_URL: process.env.BASE_URL,
     GIT_COMMIT_REF: process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF,
     GIT_COMMIT_SHA: process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA,
     VERCEL_URL: process.env.NEXT_PUBLIC_VERCEL_URL
+  },
+  sentry: {
+    disableServerWebpackPlugin: true,
+    disableClientWebpackPlugin: true
   }
 }
+
+const sentryWebpackPluginOptions = {
+  silent: true
+}
+
+module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions)
