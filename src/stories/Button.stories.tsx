c6fe6419b2be175f86436816f5bbbec151a2c8f5
Yogi <yoginth@hey.com>
2021-09-25 15:36:42 +0000

    add: Storybook

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -4,34 +4,15 @@ import React from 'react'
 import { Button } from './Button'
 
 export default {
-  title: 'Example/Button',
-  component: Button,
-  argTypes: {
-    backgroundColor: { control: 'color' }
-  }
+  title: 'Button',
+  component: Button
 } as ComponentMeta<typeof Button>
 
-const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />
+const Template: ComponentStory<typeof Button> = (args) => (
+  <Button {...args}>Hello, World</Button>
+)
 
 export const Primary = Template.bind({})
 Primary.args = {
-  primary: true,
-  label: 'Button'
-}
-
-export const Secondary = Template.bind({})
-Secondary.args = {
-  label: 'Button'
-}
-
-export const Large = Template.bind({})
-Large.args = {
-  size: 'large',
-  label: 'Button'
-}
-
-export const Small = Template.bind({})
-Small.args = {
-  size: 'small',
-  label: 'Button'
+  variant: 'primary'
 }
--- src/stories/Button.tsx
+++ src/stories/Button.tsx
@@ -1,51 +1,71 @@
-import React from 'react'
+import clsx from 'clsx'
+import React, {
+  ButtonHTMLAttributes,
+  DetailedHTMLProps,
+  forwardRef
+} from 'react'
 
-interface ButtonProps {
-  /**
-   * Is this the principal call to action on the page?
-   */
-  primary?: boolean
-  /**
-   * What background color to use
-   */
-  backgroundColor?: string
-  /**
-   * How large should the button be?
-   */
-  size?: 'small' | 'medium' | 'large'
-  /**
-   * Button contents
-   */
-  label: string
-  /**
-   * Optional click handler
-   */
-  onClick?: () => void
+interface Props
+  extends DetailedHTMLProps<
+    ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  > {
+  size?: 'sm' | 'md' | 'lg'
+  variant?: 'primary' | 'secondary' | 'success' | 'danger'
+  outline?: boolean
+  loading?: boolean
+  icon?: React.ReactNode
+  children?: React.ReactNode
+  className?: string
 }
 
-/**
- * Primary UI component for user interaction
- */
-export const Button = ({
-  primary = false,
-  size = 'medium',
-  backgroundColor,
-  label,
-  ...props
-}: ButtonProps) => {
-  const mode = primary
-    ? 'storybook-button--primary'
-    : 'storybook-button--secondary'
+export const Button = forwardRef<HTMLButtonElement, Props>(function Button(
+  {
+    className = '',
+    size = 'md',
+    variant = 'primary',
+    outline,
+    loading,
+    icon,
+    children,
+    ...rest
+  },
+  ref
+) {
   return (
     <button
-      type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(
-        ' '
+      ref={ref}
+      className={clsx(
+        {
+          'bg-brand-500 hover:bg-brand-600 border border-brand-600 text-white focus:ring-brand-400':
+            !outline && variant === 'primary',
+          'bg-gray-500 hover:bg-gray-600 border border-gray-600 text-white focus:ring-gray-400':
+            !outline && variant === 'secondary',
+          'bg-green-500 hover:bg-green-400 border border-green-600 text-white focus:ring-green-400':
+            !outline && variant === 'success',
+          'bg-red-500 hover:bg-red-400 border border-red-600 text-white focus:ring-red-400':
+            !outline && variant === 'danger',
+          'border border-brand-500 text-brand-500 hover:bg-brand-100 focus:ring-brand-400':
+            outline && variant === 'primary',
+          'border border-gray-500 text-gray-500 hover:bg-gray-100 focus:ring-gray-400':
+            outline && variant === 'secondary',
+          'border border-green-500 text-green-500 hover:bg-green-100 focus:ring-green-400':
+            outline && variant === 'success',
+          'border border-red-500 text-red-500 hover:bg-red-100 focus:ring-red-400':
+            outline && variant === 'danger',
+          'px-2 py-0.5': size === 'sm',
+          'px-3 py-1': size === 'md',
+          'px-4 py-1.5': size === 'lg',
+          'flex items-center space-x-1.5': icon && children
+        },
+        'rounded-lg font-bold disabled:opacity-50 shadow-sm focus:ring-2 focus:ring-opacity-50 focus:ring-offset-1 outline-none',
+        className
       )}
-      style={{ backgroundColor }}
-      {...props}
+      disabled={loading}
+      {...rest}
     >
-      {label}
+      {icon}
+      <div>{children}</div>
     </button>
   )
-}
+})
