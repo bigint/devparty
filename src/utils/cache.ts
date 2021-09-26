f661c032f062e7a7e27428400e5172c189a4d6e0
Yogi <yoginth@hey.com>
2021-09-26 14:23:12 +0530

    test: Cache

--- src/utils/cache.ts
+++ src/utils/cache.ts
@@ -3,6 +3,6 @@ import { GetServerSidePropsContext } from 'next'
 export async function cacheRequest(context: GetServerSidePropsContext) {
   return context.res.setHeader(
     'Cache-Control',
-    'private, s-maxage=10, stale-while-revalidate=59'
+    'public, s-maxage=10, stale-while-revalidate=59'
   )
 }
