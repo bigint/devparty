b70c724da097d5f2a3f42b6598fbe2968a310d2e
Yogi <yoginth@hey.com>
2021-09-29 04:50:08 +0000

    update: Configs

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,18 @@
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
+require('@nomiclabs/hardhat-waffle')
+
 module.exports = {
-  solidity: '0.7.3'
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    mumbai: {
+      url: 'https://polygon-mumbai.infura.io/v3/3d19324a72854976a7160e0e2ebc9c2b',
+      accounts: process.env.MM_PRIVATE_KEY
+    },
+    matic: {
+      url: 'https://polygon-mainnet.infura.io/v3/3d19324a72854976a7160e0e2ebc9c2b',
+      accounts: process.env.MM_PRIVATE_KEY
+    }
+  },
+  solidity: '0.8.4'
 }
