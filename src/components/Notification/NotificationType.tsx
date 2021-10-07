66d7da9e589aaa0ad3f5d46eb74a3c68d8d94bca
Yogi <me@yogi.codes>
2021-10-07 17:10:11 +0530

    refactor: Props

--- src/components/Notification/NotificationType.tsx
+++ src/components/Notification/NotificationType.tsx
@@ -7,34 +7,34 @@ interface Props {
   setIsRead: React.Dispatch<React.SetStateAction<boolean>>
 }
 
-const NotificationType: React.FC<Props> = ({ isRead, setIsRead }) => {
+const NotificationType: React.FC<Props> = (props: Props) => {
   return (
     <div className="flex gap-3 px-2 sm:px-0">
       <button
         className={clsx(
           {
             'text-blue-500 bg-blue-100 dark:bg-opacity-20 bg-opacity-100':
-              !isRead
+              !props.isRead
           },
           'flex items-center space-x-2 rounded-lg px-3 py-1 text-blue-500 hover:bg-blue-100 hover:text-blue-500 dark:hover:bg-opacity-20 hover:bg-opacity-100'
         )}
-        onClick={() => setIsRead(false)}
+        onClick={() => props.setIsRead(false)}
       >
         <InboxIcon className="h-4 w-4" />
-        <div className={!isRead ? '' : `hidden sm:block`}>Inbox</div>
+        <div className={!props.isRead ? '' : `hidden sm:block`}>Inbox</div>
       </button>
       <button
         className={clsx(
           {
             'text-green-500 bg-green-100 dark:bg-opacity-20 bg-opacity-100':
-              isRead
+              props.isRead
           },
           'flex items-center space-x-2 rounded-lg px-3 py-1 text-green-500 hover:bg-green-100 hover:text-green-500 dark:hover:bg-opacity-20 hover:bg-opacity-100'
         )}
-        onClick={() => setIsRead(true)}
+        onClick={() => props.setIsRead(true)}
       >
         <CheckCircleIcon className="h-4 w-4" />
-        <div className={isRead ? '' : `hidden sm:block`}>Done</div>
+        <div className={props.isRead ? '' : `hidden sm:block`}>Done</div>
       </button>
     </div>
   )
