c945cf3084035ee18a2d420548c065581703ada9
Yogi <me@yogi.codes>
2021-10-07 17:09:44 +0530

    refactor: Props

--- src/components/Home/FeedType.tsx
+++ src/components/Home/FeedType.tsx
@@ -12,34 +12,36 @@ interface Props {
   feedType: string
 }
 
-const FeedType: React.FC<Props> = ({ setFeedType, feedType }) => {
+const FeedType: React.FC<Props> = (props: Props) => {
   return (
     <div className="flex gap-3 px-2 sm:px-0">
       <button
         className={clsx(
           {
             'text-blue-500 bg-blue-100 dark:bg-opacity-20 bg-opacity-100':
-              feedType === 'ALL'
+              props.feedType === 'ALL'
           },
           'flex items-center space-x-2 rounded-lg px-3 py-1 text-blue-500 hover:bg-blue-100 hover:text-blue-500 dark:hover:bg-opacity-20 hover:bg-opacity-100'
         )}
-        onClick={() => setFeedType('ALL')}
+        onClick={() => props.setFeedType('ALL')}
       >
         <GlobeIcon className="h-4 w-4" />
-        <div className={feedType === 'ALL' ? '' : `hidden sm:block`}>All</div>
+        <div className={props.feedType === 'ALL' ? '' : `hidden sm:block`}>
+          All
+        </div>
       </button>
       <button
         className={clsx(
           {
             'text-green-500 bg-green-100 dark:bg-opacity-20 bg-opacity-100':
-              feedType === 'POST'
+              props.feedType === 'POST'
           },
           'flex items-center space-x-2 rounded-lg px-3 py-1 text-green-500 hover:bg-green-100 hover:text-green-500 dark:hover:bg-opacity-20 hover:bg-opacity-100'
         )}
-        onClick={() => setFeedType('POST')}
+        onClick={() => props.setFeedType('POST')}
       >
         <CollectionIcon className="h-4 w-4" />
-        <div className={feedType === 'POST' ? '' : `hidden sm:block`}>
+        <div className={props.feedType === 'POST' ? '' : `hidden sm:block`}>
           Posts
         </div>
       </button>
@@ -47,14 +49,14 @@ const FeedType: React.FC<Props> = ({ setFeedType, feedType }) => {
         className={clsx(
           {
             'text-pink-500 bg-pink-100 dark:bg-opacity-20 bg-opacity-100':
-              feedType === 'TASK'
+              props.feedType === 'TASK'
           },
           'flex items-center space-x-2 rounded-lg px-3 py-1 text-pink-500 hover:bg-pink-100 hover:text-pink-500 dark:hover:bg-opacity-20 hover:bg-opacity-100'
         )}
-        onClick={() => setFeedType('TASK')}
+        onClick={() => props.setFeedType('TASK')}
       >
         <CheckCircleIcon className="h-4 w-4" />
-        <div className={feedType === 'TASK' ? '' : `hidden sm:block`}>
+        <div className={props.feedType === 'TASK' ? '' : `hidden sm:block`}>
           Tasks
         </div>
       </button>
@@ -62,14 +64,14 @@ const FeedType: React.FC<Props> = ({ setFeedType, feedType }) => {
         className={clsx(
           {
             'text-purple-500 bg-purple-100 dark:bg-opacity-20 bg-opacity-100':
-              feedType === 'QUESTION'
+              props.feedType === 'QUESTION'
           },
           'flex items-center space-x-2 rounded-lg px-3 py-1 text-purple-500 hover:bg-purple-100 hover:text-purple-500 dark:hover:bg-opacity-20 hover:bg-opacity-100'
         )}
-        onClick={() => setFeedType('QUESTION')}
+        onClick={() => props.setFeedType('QUESTION')}
       >
         <QuestionMarkCircleIcon className="h-4 w-4" />
-        <div className={feedType === 'QUESTION' ? '' : `hidden sm:block`}>
+        <div className={props.feedType === 'QUESTION' ? '' : `hidden sm:block`}>
           Question
         </div>
       </button>
