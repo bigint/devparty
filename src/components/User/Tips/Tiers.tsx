84998d7354a81b78b37c0c6899c96b798b2db9cf
Sasi <reachsasidharank@gmail.com>
2021-11-23 16:05:55 +0000

    Draft: Tip UI

--- src/components/User/Tips/Tiers.tsx
+++ src/components/User/Tips/Tiers.tsx
@@ -11,8 +11,7 @@ const TipTiers: React.FC<Props> = ({ tiers }) => {
   return (
     <div className="mt-5">
       <div className="text-lg font-bold">Tip via Devparty</div>
-      <div className="mt-5 border-b" />
-      <div className="!mt-0 divide-y">
+      <div className="mt-4">
         {tiers?.map((tier) => (
           <SingleTier key={tier?.id} tier={tier as TipTier} />
         ))}
